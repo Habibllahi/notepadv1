0f5d97f032a59736452e29140093caeaadd8e7aa
Hamzat Habibllahi Adewale <habibllahi3@gmail.com>
2021-09-25 18:00:53 +0100

    CORS global filter enabled in WebConfig

--- src/main/java/ng/com/codetrik/notepad/WebConfig.java
+++ src/main/java/ng/com/codetrik/notepad/WebConfig.java
@@ -5,6 +5,7 @@ package ng.com.codetrik.notepad;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -19,5 +20,15 @@ public class WebConfig implements WebMvcConfigurer {
         vr.setPrefix("/WEB-INF/JSP/");
         vr.setSuffix(".jsp");
         return vr;
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/v1/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET","POST","PUT","DELETE")
+                .allowedHeaders("*")
+                .exposedHeaders("*")
+                .maxAge(3600);
     }
 }
