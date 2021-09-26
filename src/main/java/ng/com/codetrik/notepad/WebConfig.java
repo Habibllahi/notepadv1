85613aa7a3679f53a817a7ec017d46a2ea3ec9cf
Hamzat Habibllahi Adewale <habibllahi3@gmail.com>
2021-09-26 13:55:36 +0100

    all methods alloed for CORS

--- src/main/java/ng/com/codetrik/notepad/WebConfig.java
+++ src/main/java/ng/com/codetrik/notepad/WebConfig.java
@@ -24,9 +24,9 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/v1/*")
+        registry.addMapping("/api/v1/**")
                 .allowedOrigins("*")
-                .allowedMethods("GET","POST","PUT","DELETE")
+                .allowedMethods("*")
                 .allowedHeaders("*")
                 .exposedHeaders("*")
                 .maxAge(3600);
